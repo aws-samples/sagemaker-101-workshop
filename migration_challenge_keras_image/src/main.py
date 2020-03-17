02803eba492f2ae4d49ac805d0efb0cabe43f33f
Alex Thewsey <athewsey@gmail.com>
2020-03-17 12:38:53 +0000

    feat(migrate): extend instructions for clarity
    More guidance on using the model; simpler save method

--- migration_challenge_keras_image/src/main.py
+++ migration_challenge_keras_image/src/main.py
@@ -2,20 +2,23 @@
 
 # Dependencies:
 import argparse
-# TODO
+# TODO: Others?
 
-# TODO: Function definitions as needed?
+def parse_args():
+    # TODO: Standard pattern for loading parameters in from SageMaker
+
+# TODO: Other function definitions, if you'd like to break up your code?
 
 # Training script:
 if __name__ == "__main__":
-    # Load arguments from CLI / environment variables?
-    
-    # Load images from container filesystem into training / test data sets?
-    
-    # Create the Keras model?
-    
-    # Fit the Keras model?
-    
-    # Evaluate model quality and log metrics?
-    
-    # Save outputs (e.g. trained model) to specified folder(s)?
+    # TODO: Load arguments from CLI / environment variables?
+
+    # TODO: Load images from container filesystem into training / test data sets?
+
+    # TODO: Create the Keras model?
+
+    # TODO: Fit the Keras model?
+
+    # TODO: Evaluate model quality and log metrics?
+
+    # TODO: Save outputs (trained model) to specified folder?
