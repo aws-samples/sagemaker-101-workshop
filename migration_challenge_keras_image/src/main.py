baf94944ecef7c6cf24b3a46511abbef7796500a
Alex Thewsey <athewsey@gmail.com>
2021-02-08 14:35:01 +0000

    doc(migrate): extend code templates a little
    Add a couple of extra pointers in the instructions & templates to
    try and boost completion in recognition of SMStudio lacking local
    mode.

--- migration_challenge_keras_image/src/main.py
+++ migration_challenge_keras_image/src/main.py
@@ -1,4 +1,7 @@
-"""CNN-based image classification on SageMaker with TensorFlow and Keras"""
+"""CNN-based image classification on SageMaker with TensorFlow and Keras
+
+(Complete me with help from Local Notebook.ipynb, and the NLP example's src/main.py!)
+"""
 
 # Dependencies:
 import argparse
@@ -7,18 +10,25 @@ import argparse
 def parse_args():
     # TODO: Standard pattern for loading parameters in from SageMaker
 
-# TODO: Other function definitions, if you'd like to break up your code?
+# TODO: Other function definitions, if you'd like to break up your code into functions?
 
 # Training script:
 if __name__ == "__main__":
-    # TODO: Load arguments from CLI / environment variables?
+    # Load arguments from CLI / environment variables:
+    args, unknown = parse_args()
 
     # TODO: Load images from container filesystem into training / test data sets?
 
     # TODO: Create the Keras model?
 
-    # TODO: Fit the Keras model?
+    # Fit the Keras model:
+    model.fit(
+        ?
+    )
 
     # TODO: Evaluate model quality and log metrics?
 
     # TODO: Save outputs (trained model) to specified folder?
+    tf.saved_model.simple_save(
+        ?
+    )
