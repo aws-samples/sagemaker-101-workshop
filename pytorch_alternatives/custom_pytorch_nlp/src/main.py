2aac022d9aea91f76f9c2f34281ffd52b5212e60
Alex Thewsey <thewsey@amazon.com>
2021-08-12 09:25:12 +0000

    feat(nlp): update PyTorch alt NLP to FastText
    Update PyTorch alternative for NLP exercise to align to the FastText
    word embeddings now used in the TF.Keras version. Tweak eval batch
    size for faster compute. Adjust loss metrics for better train/val
    consistency. Align default instance type to g4dn.xlarge.

--- pytorch_alternatives/custom_pytorch_nlp/src/main.py
+++ pytorch_alternatives/custom_pytorch_nlp/src/main.py
@@ -19,6 +19,27 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
+###### Define the model ############
+class Net(nn.Module):
+    def __init__(self, vocab_size=400000, emb_dim=300, num_classes=4):
+        super(Net, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.conv1 = nn.Conv1d(emb_dim, 128, kernel_size=3)
+        self.max_pool1d = nn.MaxPool1d(5)
+        self.flatten1 = nn.Flatten()
+        self.dropout1 = nn.Dropout(p=0.3)
+        self.fc1 = nn.Linear(896, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+    
+    def forward(self, x):
+        x = self.embedding(x)  
+        x = torch.transpose(x,1,2)
+        x = self.flatten1(self.max_pool1d(self.conv1(x)))
+        x = self.dropout1(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=-1)
+
 ###### Helper functions ############
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, data, labels):
@@ -58,7 +79,7 @@ def parse_args():
     parser.add_argument("--epochs", type=int, default=1)
     parser.add_argument("--learning_rate", type=float, default=0.001)
     parser.add_argument("--num_classes", type=int, default=4)
-    parser.add_argument("--vocab_size", type=int, default=400000)
+    parser.add_argument("--max_seq_len", type=int, default=40)
 
     # Data, model, and output directories
     parser.add_argument("--output-data-dir", type=str, default=os.environ.get("SM_OUTPUT_DATA_DIR"))
@@ -69,26 +90,6 @@ def parse_args():
 
     return parser.parse_known_args()
 
-class Net(nn.Module):
-    def __init__(self, vocab_size = 400000, num_classes = 4):
-        super(Net, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, 100)
-        self.conv1 = nn.Conv1d(100, 128, kernel_size=3)
-        self.max_pool1d = nn.MaxPool1d(5)
-        self.flatten1 = nn.Flatten()
-        self.dropout1 = nn.Dropout(p=0.3)
-        self.fc1 = nn.Linear(896, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = self.embedding(x)  
-        x = torch.transpose(x,1,2)
-        x = self.flatten1(self.max_pool1d(self.conv1(x)))
-        x = self.dropout1(x)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.softmax(x, dim=-1)
-
 def test(model, test_loader, device):
     model.eval()
     test_loss = 0.0
@@ -97,14 +98,13 @@ def test(model, test_loader, device):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.binary_cross_entropy(output, target, reduction="mean").item()  # sum up batch loss
+            test_loss += F.binary_cross_entropy(output, target, reduction="sum").item()
             pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
             target_index = target.max(1, keepdim=True)[1]
             correct += pred.eq(target_index).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-    print("val_loss: {:.4f}".format(test_loss))
-    print("val_acc: {:.4f}".format(correct/len(test_loader.dataset)))   
+    test_loss /= len(test_loader.dataset)  # Average loss over dataset samples
+    print(f"val_loss: {test_loss:.4f}, val_acc: {correct/len(test_loader.dataset):.4f}")
 
 def train(args):
     ###### Load data from input channels ############
@@ -113,7 +113,11 @@ def train(args):
     embedding_matrix = load_embeddings(args.embeddings)
 
     ###### Setup model architecture ############
-    model = Net(args.vocab_size, args.num_classes)
+    model = Net(
+        vocab_size=embedding_matrix.shape[0],
+        emb_dim=embedding_matrix.shape[1],
+        num_classes=args.num_classes,
+    )
     model.embedding.weight = torch.nn.parameter.Parameter(torch.FloatTensor(embedding_matrix), False)
     device = torch.device("cpu")
     if torch.cuda.is_available():
@@ -124,6 +128,7 @@ def train(args):
     for epoch in range(1, args.epochs + 1):
         model.train()
         running_loss = 0.0
+        n_batches = 0
         for batch_idx, (X_train, y_train) in enumerate(train_loader, 1):
             data, target = X_train.to(device), y_train.to(device)
             optimizer.zero_grad()
@@ -132,21 +137,25 @@ def train(args):
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-        print("epoch: {}".format(epoch))
-        print("train_loss: {:.6f}".format(running_loss / (len(train_loader.dataset))))     
+            n_batches += 1
+        print(f"epoch: {epoch}, train_loss: {running_loss / n_batches:.6f}")  # (Avg over batches)
         print("Evaluating model")
         test(model, test_loader, device)
-    save_model(model, args.model_dir)
+    save_model(model, args.model_dir, args.max_seq_len)
 
-def save_model(model, model_dir):
+def save_model(model, model_dir, max_seq_len):
     path = os.path.join(model_dir, "model.pth")
-    x = torch.randint(0,10,(1,40))
+    x = torch.randint(0, 10, (1, max_seq_len))
     model = model.cpu()
     model.eval()
     m = torch.jit.trace(model, x)
     torch.jit.save(m, path)
 
 def model_fn(model_dir):
+    """Customized model loading function for inference
+
+    https://sagemaker.readthedocs.io/en/stable/frameworks/pytorch/using_pytorch.html#load-a-model
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.jit.load(os.path.join(model_dir, "model.pth")).to(device)
     return model
